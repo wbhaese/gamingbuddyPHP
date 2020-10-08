7e0fb46f03f36b38253828355bdde6fdd96e2f91
WallaceBH <46716156+wbhaese@users.noreply.github.com>
2020-10-08 11:30:21 -0300

    Add query to create a new table user

---
+++ sqlQuery.sql
@@ -0,0 +1,2 @@
+
+CREATE TABLE `gameTest`.`user` ( `id` INT NOT NULL , `name` varchar(255) NOT NULL , `email` varchar(255) NOT NULL , `pswd` varchar(255) NOT NULL,  `description` varchar(255) , PRIMARY KEY (`id`)) ENGINE = InnoDB;
